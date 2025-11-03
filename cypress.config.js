769c3a33da2592169eb510de4805707a46933f5c
MxnlodyZ <wattanachai.work@hotmail.com>
2025-11-03 19:24:06 +0700

    fix(cypress): add xpath and file dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 export default {
   e2e: {
-    baseUrl: 'http://localhost:8080/', // URL ที่รัน frontend อยู่
+    baseUrl: 'http://localhost:3000/', // URL ที่รัน frontend อยู่
     video: true,
     screenshotOnRunFailure: true,
     viewportWidth: 1440,
